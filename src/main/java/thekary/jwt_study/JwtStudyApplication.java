a53e0d895211b55345c7f44b0bb343c918a7baa3
vkfkstptkd1 <vkfkstptkd1@naver.com>
2022-10-25 18:18:13 +0900

    backend 준비

---
+++ src/main/java/thekary/jwt_study/JwtStudyApplication.java
@@ -0,0 +1,13 @@
+package thekary.jwt_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/thekary/jwt_study/JwtStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package thekary.jwt_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
